8e9f9d2476bf335a7ee1a67a46802ecd9fbf5359
Radim Kohout <radimm.kohout@gmail.com>
2023-11-27 22:39:12 +0100

    Update createdb.sh

--- createdb.sh
+++ createdb.sh
@@ -14,9 +14,10 @@ carto -v
 carto project.mml > mapnik.xml
 mkdir ~/data
 cd ~/data
-wget https://download.geofabrik.de/europe/czech-republic-latest.osm.pbf
+#wget https://download.geofabrik.de/europe/czech-republic-latest.osm.pbf
+wget https://download.geofabrik.de/europe-latest.osm.pbf
 chmod o+rx ~
-sudo -u _renderd osm2pgsql -d gis --create --slim  -G --hstore --tag-transform-script ~/src/openstreetmap-carto/openstreetmap-carto.lua -C 2500 --number-processes 2 -S ~/src/openstreetmap-carto/openstreetmap-carto.style ~/data/czech-republic-latest.osm.pbf
+sudo -u _renderd osm2pgsql -d gis --create --slim  -G --hstore --tag-transform-script ~/src/openstreetmap-carto/openstreetmap-carto.lua -C 2500 --number-processes 2 -S ~/src/openstreetmap-carto/openstreetmap-carto.style ~/data/europe-latest.osm.pbf
 cd ~/src/openstreetmap-carto/
 sudo -u _renderd psql -d gis -f indexes.sql
 cd ~/src/openstreetmap-carto/
